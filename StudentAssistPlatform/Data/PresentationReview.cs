555b7c520200be2519bd2ff16977d0bc2ae20a99
idan gomberg <idangomberg@gmail.com>
2025-01-18 03:02:07 -0500

    Add project files.

---
+++ StudentAssistPlatform/Data/PresentationReview.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentAssistPlatform.Data
+{
+    public class PresentationReview
+    {
+        public int Id { get; set; }
+        public int PresentationId { get; set; }
+        public string ReviewerId { get; set; }
+        public string Review { get; set; }
+        public int Rating { get; set; }
+        public string ReviewerName { get; set; }
+        public string ReviewerEmail { get; set; }
+        public string ReviewerProfilePicture { get; set; }
+    }
+}
